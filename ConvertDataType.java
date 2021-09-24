56499d8a9e22399934475ad180888486ef3a7bf9
wandanuriza <wanda.nuriza.tif20@polban.ac.id>
2021-09-24 07:09:55 +0700

    menambahkan script java untuk tugas praktikum kedua

---
+++ ConvertDataType.java
@@ -0,0 +1,16 @@
+package tugas;
+
+public class ConvertDataType {
+	static short methodOne(long l) { 
+		int i = (int) l; 
+		return (short)i;  
+		} 
+	public static void main(String[] args) { 
+		double d = 10.25; 
+		float f = (float) d;  
+		byte b = (byte) methodOne((long) f);  
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(b);
+		} 
+}
---
+++ FloatingPoint.java
@@ -0,0 +1,11 @@
+package tugas;
+
+public class FloatingPoint {
+	public static void main(String[] args) {
+		double x = 92.98;  
+		int nx = (int) Math.round(x); 
+		System.out.println("Nilai x adalah " + x);
+		System.out.println("Nilai nx adalah " + nx);
+	}
+	
+}
